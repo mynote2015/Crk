e384a807e10b0e95ceb5c34b0ddf8c93bc82e839
mynote2015 <mynote2015@yeah.net>
2017-12-13 09:55:08 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -63,3 +63,4 @@ cd $CURRENTPATH
 
 wget https://people.debian.org/~aurel32/qemu/powerpc/debian_wheezy_powerpc_standard.qcow2
 wget https://people.debian.org/~aurel32/qemu/mips/debian_wheezy_mips_standard.qcow2
+wget https://people.debian.org/~aurel32/qemu/mips/vmlinux-3.2.0-4-4kc-malta
